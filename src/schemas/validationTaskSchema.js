65528188d02a88c8163334961a4f775ffe83746a
yerlinson <53019615+yerlinson10@users.noreply.github.com>
2024-09-19 14:58:50 -0400

    fix

--- src/schemas/validationTaskSchema.js
+++ src/schemas/validationTaskSchema.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const schemaTask = yup.object({
-  title: yup.string().min(2).max(50).required(),
-  description: yup.string().min(2).max(255),
+  title: yup.string().min(2).max(100).required(),
+  description: yup.string().min(2).max(500),
 });
